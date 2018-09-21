df2495d0f17c6b7d952461263c80bfaa961b8fdc
datouaizi <345316222@qq.com>
2018-09-21 22:40:13 +0800

    MEDIA PICKER

--- android/src/main/kotlin/com/jasmine/mediapicker/MediaPickerPlugin.kt
+++ android/src/main/kotlin/com/jasmine/mediapicker/MediaPickerPlugin.kt
@@ -8,27 +8,27 @@ import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 class MediaPickerPlugin private constructor(private var registrar: PluginRegistry.Registrar,private val delegate:MediaPickerDelegate) : MethodCallHandler {
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "media_picker")
-      val delegate = MediaPickerDelegate(registrar.activity())
-      val plugin = MediaPickerPlugin(registrar,delegate)
-      registrar.addActivityResultListener(delegate)
-      channel.setMethodCallHandler(plugin)
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            val channel = MethodChannel(registrar.messenger(), "media_picker")
+            val delegate = MediaPickerDelegate(registrar.activity())
+            val plugin = MediaPickerPlugin(registrar,delegate)
+            registrar.addActivityResultListener(delegate)
+            channel.setMethodCallHandler(plugin)
+        }
     }
-  }
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (registrar.activity() == null) {
-      result.error("no_activity", "image_picker plugin requires a foreground activity.", null)
-      return
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        if (registrar.activity() == null) {
+            result.error("no_activity", "image_picker plugin requires a foreground activity.", null)
+            return
+        }
+        when {
+            call.method == "pick" -> {
+                delegate.initPicker(call,result)
+            }
+            else -> result.notImplemented()
+        }
     }
-    when {
-      call.method == "pick" -> {
-        delegate.initPicker(call,result)
-      }
-      else -> result.notImplemented()
-    }
-  }
 }
